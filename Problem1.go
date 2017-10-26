9aa939e007973f63ab8ff96077443f453f50be7c
DAVID CLARKE - Student <G00335563@gmit.ie>
2017-10-26 19:55:44 +0100

    Problem 4

--- Problem1.go
+++ Problem1.go
@@ -1,25 +1,63 @@
-// Problem 2
+// Problem 4
 // David Clarke
-// Websites used https://getbootstrap.com/docs/4.0/getting-started/introduction/#starter-template
+// Websites used https://stackoverflow.com/questions/2906582/how-to-create-an-html-button-that-acts-like-a-link
 
 package main
-
+//imports
 import (
-//	"fmt"
+	"time"
 	"net/http"
+	"math/rand"
+	"html/template"
+	"strconv"
 )
+
+type myMsg struct {
+    Message string
+}
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	//Changes the font to Bold
 	w.Header().Set("Content-Type","text/html")
 
-	//Takes the HTML file "problem3.html" and outputs it in the web app
-	http.ServeFile(w, r, "problem3.html")
+	//Takes the HTML file "guess.html" and outputs it in the web app
+	http.ServeFile(w, r, "guess.html")
 
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request){
+
+		message :="Guess a number between 1 and 20"
+		
+			rand.Seed(time.Now().UTC().UnixNano())
+		
+			target:=0
+			var cookie, err = r.Cookie("target")
+		
+			if err == nil{
+				
+				target, _ = strconv.Atoi(cookie.Value)
+				if target ==0{
+					target = rand.Intn(20-1)
+				}
+			}
+		
+			cookie = &http.Cookie{
+				Name: "target",
+				Value: strconv.Itoa(target),
+				Expires: time.Now().Add(72 * time.Hour),
+			}
+			
+			http.SetCookie(w,cookie)
+			
+			t, _ := template.ParseFiles("guess.tmpl")
+
+			t.Execute(w, &myMsg{Message:message})
 }
 
 func main() {
 	http.HandleFunc("/", requestHandler)
 	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/guess", guessHandler)
 }
